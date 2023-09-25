c75dd71f4fb15d5ec13ab05e5b995c2030e6b2c3
Cagkansari <cagkansari@hotmail.com>
2023-09-25 09:45:48 +0300

    three subscriber objects

--- NATS_Compact_GTest.cpp
+++ NATS_Compact_GTest.cpp
@@ -22,15 +22,23 @@ public:
 
 
 TEST(NATSGTest, TestPublishSubscribe) {
-NATS_Compact_Test x;
+NATS_Compact_Test x,y,z;
 boost::thread pubThread(&NATS_Compact_Test::PublishingMsg, &x);
-boost::thread subThread(&NATS_Compact_Test::SubscribingForMsg, &x);
+boost::thread subThread1(&NATS_Compact_Test::SubscribingForMsg, &x);
+boost::thread subThread2(&NATS_Compact_Test::SubscribingForMsg, &y);
+boost::thread subThread3(&NATS_Compact_Test::SubscribingForMsg, &z);
 pubThread.join();
-subThread.join();
+subThread1.join();
+subThread2.join();
+subThread3.join();
 
 
  ASSERT_GE(x.receivedMsgCount, 1000);
  ASSERT_LE(x.subTimeElapsed, 1000000);
+  ASSERT_GE(y.receivedMsgCount, 1000);
+ ASSERT_LE(y.subTimeElapsed, 1000000);
+  ASSERT_GE(z.receivedMsgCount, 1000);
+ ASSERT_LE(z.subTimeElapsed, 1000000);
 }
 
 int main(int argc, char** argv) {
